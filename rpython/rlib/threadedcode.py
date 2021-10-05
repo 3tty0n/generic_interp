26921e3edbf730a318208f450544eabcbd321d4f
Yusuke Izawa <me@yizawa.com>
2021-10-05 23:19:37 +0900

    Added PoC library

---
+++ rpython/rlib/threadedcode.py
@@ -0,0 +1,15 @@
+class Transformer(object):
+    def __init__(self, pc=None):
+        self.pc = pc
+
+    def transform_jump(self, cond, true_path, false_path, **kwargs):
+        return None
+
+    def transform_ret(self, ret_value, **kwargs):
+        return None
+
+    def transform_branch(self, cond, true_path, false_path, **kwargs):
+        return None
+
+def we_are_not_translated(kind):
+    return True
